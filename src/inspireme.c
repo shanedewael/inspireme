b8fecb6be1a5e208abe167e941fa8ecb8c2bb7fe
Shane DeWael <dewaels@purdue.edu>
2015-01-18 05:35:47 -0500

    Kind of working?

--- src/inspireme.c
+++ src/inspireme.c
@@ -1,38 +1,44 @@
 #include <pebble.h>
 
-#define NUM_MENU_ITEMS 5
-  
 static Window *window;
 static Window *inspiration;
 static TextLayer *times;
 static TextLayer *dates;
-static TextLayer *caltext;
 static TextLayer *inspire;
+static AppSync sync;
+static uint8_t sync_buffer[150];
+
+enum InspirationKeys {
+  QUOTE_TEMP_KEY = 0x0
+};
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   if (window_is_loaded(window)) {
     window_stack_push(inspiration, true);
   }
 }
-      
+
 static void click_config_provider(void *context) {
-  // Register the ClickHandlers
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
+
+static void sync_error(DictionaryResult dict_error, 
+ AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_success(const uint32_t key, const Tuple* new_tuple, 
+ const Tuple* old_tuple, void* context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Sync Success %s", new_tuple->value->cstring);
+  text_layer_set_text(inspire, new_tuple->value->cstring);
+}
+
 static void update_time() {
-  // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-
-  // Create a long-lived buffer
   static char buffer[] = "00:00";
-
-
   strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-
-  // Display this time on the TextLayer
   text_layer_set_text(times, buffer);
-
 }
 
 static void update_date() {
@@ -43,88 +49,62 @@ static void update_date() {
   text_layer_set_text(dates, buffer);
 }
 
-
-
-
 static void main_window_load(Window *window) {
-  // Create time TextLayer
   times = text_layer_create(GRect(-25, 60, 144, 100));
   text_layer_set_background_color(times, GColorClear);
   text_layer_set_text_color(times, GColorWhite);
   text_layer_set_text(times, "00:00");
-  
-  // Create date TextLayer
   dates = text_layer_create(GRect(-30, 100, 144, 50));
   text_layer_set_background_color(dates, GColorClear);
   text_layer_set_text_color(dates, GColorWhite);
   text_layer_set_text(dates, "00:00");
-
-  // Improve the layout to be more like a watchface
   text_layer_set_font(times, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(times, GTextAlignmentRight);
-
   text_layer_set_font(dates, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   text_layer_set_text_alignment(dates, GTextAlignmentRight);
-
-  // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(times));
-  
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(dates));
-  
-  // Add click handler
   window_set_click_config_provider(window, click_config_provider);
-
-  
-  // Make sure the time is displayed from the start
   update_time();
-  
-  //Update date
   update_date();
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
   text_layer_destroy(times);
   text_layer_destroy(dates);
 }
 
 static void insp_window_load(Window *inspiration) {
-  //The static time layer
-  inspire = text_layer_create(GRect(0, 0, 144, 168));
+  inspire = text_layer_create(GRect(0, 0, 134, 158));
   text_layer_set_background_color(inspire, GColorBlack);
   text_layer_set_text_color(inspire, GColorWhite);
   text_layer_set_text_alignment(inspire, GTextAlignmentCenter);
-  text_layer_set_font(inspire, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(inspire, "Inspire me");
+  text_layer_set_font(inspire, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   
+  Tuplet inital_values[] = {
+    TupletCString(QUOTE_TEMP_KEY, "Loading...")
+  };
 
-
-  
+  text_layer_set_text(inspire, QUOTE_TEMP_KEY);
+  app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), inital_values,
+    ARRAY_LENGTH(inital_values), sync_success, sync_error, NULL);
   layer_add_child(window_get_root_layer(inspiration), text_layer_get_layer(inspire));
-  
-
-  
-  //Set the date
-
 } 
 
 static void insp_window_unload(Window *inspiration) {
   text_layer_destroy(inspire);
 }
 
-
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
-  
+
 static void init() {
-  // Create main Window element and assign to pointer
   window = window_create();
   window_set_background_color(window, GColorBlack);
-
   inspiration = window_create();
+  window_set_background_color(inspiration, GColorBlack);
 
-  // Set handlers to manage the elements inside the Window
   window_set_window_handlers(window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
@@ -135,29 +115,21 @@ static void init() {
     .unload = insp_window_unload
   });
 
-
+  app_message_open(150, 64);
   window_set_fullscreen(window, true);
   window_set_fullscreen(inspiration, true);
-
-  // Show the Window on the watch, with animated=true
   window_stack_push(inspiration, true);
   window_stack_push(window, true);
-  
-  
-  // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
-  // Destroy Window
   window_destroy(window);
   window_destroy(inspiration);
 }
 
 int main(void) {
   init();
-
   app_event_loop();
   deinit();
-
 }
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,18 +1,25 @@
-Pebble.addEventListener('ready',
-  function(e) {
-    var req = new XMLHttpRequest();
-req.open('GET', 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=text&lang=en', true);
-req.onload = function(e) {
-  if (req.readyState == 4 && req.status == 200) {
-    if(req.status == 200) {
-      var response = req.responseText;
-      console.log(response);
-      Pebble.showSimpleNotificationOnPebble("Sample", response);
-    } else { console.log('Error'); }
-  }
-}
-req.send(null);
-  }
-);
+Pebble.addEventListener('ready', function() {
+  getQuote();
+});
+
+//HTTP request for inspirational quote
+var getQuote = function() {
+  var request = new XMLHttpRequest();
+  request.open('GET',
+               'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en',
+               true);
+  request.onload = sendQuote;
+  request.send();
+};
+
+//Process JSON
+var sendQuote = function(xhr) {
+	var response = JSON.parse(xhr.target.response);
+	var quote = response.quoteText + "\n-" + response.quoteAuthor;
+  Pebble.sendAppMessage({
+    'motivate': quote
+  });
+};
+
 
 
